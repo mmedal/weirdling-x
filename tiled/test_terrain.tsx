bffce4e275aaa6c7b781a4a8e6d9fc952ce550de
Matthew Medal <matt.medal@gmail.com>
2024-02-20 16:17:06 -0800

    Add tiled project for level designs

---
+++ tiled/test_terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Test Terrain" tilewidth="40" tileheight="40" tilecount="14" columns="14">
+ <image source="../source/assets/images/testterrain-table-40-40.png" width="560" height="40"/>
+</tileset>
