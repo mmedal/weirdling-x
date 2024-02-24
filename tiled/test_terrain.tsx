91c977bb21e0f9738bea70ea1af93cb9ec6551fd
Matthew Medal <matt.medal@gmail.com>
2024-02-23 20:59:32 -0800

    Test adding tilemap metadata in Tiled

--- tiled/test_terrain.tsx
+++ tiled/test_terrain.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Test Terrain" tilewidth="40" tileheight="40" tilecount="14" columns="14">
  <image source="../source/assets/images/testterrain-table-40-40.png" width="560" height="40"/>
+ <tile id="0">
+  <properties>
+   <property name="boundaries" value="{ 1, 0, 0, 0 }"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="boundaries" value="{ 0, 0, 1, 0 }"/>
+  </properties>
+ </tile>
 </tileset>
